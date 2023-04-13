7cdae8cf5b6b71f8748eb777f555729df6e2023c
Angeles B <a.brrls@gmail.com>
2023-04-13 15:47:09 -0600

    First commit

---
+++ src/app/contador/contador/contador.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-contador',
+    template: `
+    <h1> {{ title }} </h1>
+    <h1> {{ base }} </h1>
+    <button (click)=" operacion (base) "> + {{ base }} </button>
+    <span> {{ num }} </span>
+    <button (click)=" operacion (-base)  "> - {{ base }} </button>
+    `  
+})
+
+
+export class ContadorComponent {
+    title : string = 'Contador App';
+    num   : number = 10;
+    base  : number = 5;
+    
+    operacion (valor: number){
+      this.num += valor;
+    }
+
+}
---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-heroe',
+    templateUrl: 'heroe.componente.html',
+})
+export class HeroeComponent{
+    name: string = 'Ironman';
+    age: number = 45;
+
+
+    get nameCapitalized(){
+        return this.name.toUpperCase();
+    }
+
+    getName(): string {
+        return `${ this.name } - ${ this.age } `
+    }
+
+    changeName(): void{
+        this.name = 'Spiderman';
+    }
+
+    changeAge(): void{
+        this.age = 39
+    }
+}
